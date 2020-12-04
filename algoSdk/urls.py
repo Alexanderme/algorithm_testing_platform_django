bb68997961b6876373144de2137f6c9dbe1cf7a3
root <470485145@qq.com>
2020-12-04 16:06:27 +0800

    first commit

--- algoSdk/urls.py
+++ algoSdk/urls.py
@@ -4,3 +4,11 @@
     #  @Author: Ljx
     #  @Time: 2020/12/4 14:34
 """
+from django.conf.urls import url
+
+from .api.IasBuild import IasPackage
+
+
+urlpatterns = [
+    url(r'user/dingConfig', IasPackage.as_view()),
+]
